1ecf08c0c6dce43e757153e865d4fe0647d62447
Nolan Carroll <ncarroll@mdsol.com>
2015-04-23 10:52:03 -0400

    initial commit

---
+++ MORKExample/MORKExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MORKExample
+//
+//  Created by Nolan Carroll on 4/23/15.
+//  Copyright (c) 2015 Medidata Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MORKExample/MORKExampleTests/MORKExampleTests.m
@@ -0,0 +1,40 @@
+//
+//  MORKExampleTests.m
+//  MORKExampleTests
+//
+//  Created by Nolan Carroll on 4/23/15.
+//  Copyright (c) 2015 Medidata Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MORKExampleTests : XCTestCase
+
+@end
+
+@implementation MORKExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
