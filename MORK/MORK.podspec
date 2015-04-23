87b41f4cb9827787a8ed0d426efdd5cb2a17c651
Nolan Carroll <ncarroll@mdsol.com>
2015-04-23 12:05:52 -0400

    add podspec, reference relative file in example podfile

---
+++ MORK/MORK.podspec
@@ -0,0 +1,6 @@
+Pod::Spec.new do |s|
+
+  s.name = "MORK"
+  s.dependency 'ResearchKit'
+
+end
--- MORKExample/MORKExample/ViewController.m
+++ MORKExample/MORKExample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ResearchKit/ResearchKit.h>
 
 @interface ViewController ()
 
@@ -23,5 +24,76 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSMutableArray *steps = [self createSteps];
+    ORKOrderedTask *task  = [[ORKOrderedTask alloc] initWithIdentifier:@"task" steps:steps];
+    ORKTaskViewController *taskViewController = [[ORKTaskViewController alloc] initWithTask:task taskRunUUID:nil];
+    
+    taskViewController.delegate = self;
+    [self presentViewController:taskViewController animated:YES completion:nil];
+}
+
+
+- (void)taskViewController:(ORKTaskViewController *)taskViewController
+       didFinishWithReason:(ORKTaskViewControllerFinishReason)reason
+                     error:(NSError *)error {
+    
+
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+-(NSMutableArray *) createSteps {
+    NSMutableArray *steps = [NSMutableArray new];
+    
+    
+    {
+        ORKInstructionStep *step = [[ORKInstructionStep alloc] initWithIdentifier:@"intro"];
+        step.title = @"Welcome to ResearchKit";
+        [steps addObject:step];
+    }
+    
+    
+    {
+        NSArray *choices = @[[ORKTextChoice choiceWithText:@"Male" detailText:nil value:@"M"],
+                             [ORKTextChoice choiceWithText:@"Female" detailText:nil value:@"F"]];
+        ORKTextChoiceAnswerFormat *textFormat = [[ORKTextChoiceAnswerFormat alloc] initWithStyle:ORKChoiceAnswerStyleSingleChoice textChoices:choices];
+        ORKQuestionStep *step = [ORKQuestionStep questionStepWithIdentifier:@"GENDER" title:@"Gender" answer:textFormat];
+        [steps addObject:step];
+    }
+    
+    
+    {
+        ORKDateAnswerFormat *dateFormat = [ORKDateAnswerFormat dateAnswerFormat];
+        ORKQuestionStep *step = [ORKQuestionStep questionStepWithIdentifier: @"DOB" title: @"When were you born?" answer: dateFormat];
+        [steps addObject:step];
+    }
+    
+    
+    {
+        NSArray *choices = @[[ORKTextChoice choiceWithText:@"Never" detailText:nil value:@"Never"],
+                             [ORKTextChoice choiceWithText:@"Rarely" detailText:nil value:@"Rarely"],
+                             [ORKTextChoice choiceWithText:@"Somewhat" detailText:nil value:@"Somewhat"],
+                             [ORKTextChoice choiceWithText:@"Often" detailText:nil value:@"Often"]];
+        ORKTextChoiceAnswerFormat *textFormat = [[ORKTextChoiceAnswerFormat alloc] initWithStyle:ORKChoiceAnswerStyleSingleChoice textChoices:choices];
+        ORKQuestionStep *step = [ORKQuestionStep questionStepWithIdentifier:@"EXERCISE" title:@"How often do you exercise?" answer:textFormat];
+        [steps addObject:step];
+    }
+    
+    
+    {
+        ORKScaleAnswerFormat *scaleFormat = [[ORKScaleAnswerFormat alloc] initWithMaximumValue:10 minimumValue:1 step:1 defaultValue:5];
+        ORKQuestionStep *step = [ORKQuestionStep questionStepWithIdentifier:@"PAIN_LEVEL" title:@"How much pain do you feel in your arm?" answer:scaleFormat];
+        [steps addObject:step];
+    }
+    
+    
+    return steps;
+}
+
+
 
 @end
