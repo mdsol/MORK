a9ac5782fc4652f5105cd17db1addfc25f293ce6
Nolan Carroll <ncarroll@mdsol.com>
2015-04-23 10:32:42 -0400

    Initial Commit

---
+++ MORK/MORK.h
@@ -0,0 +1,19 @@
+//
+//  MORK.h
+//  MORK
+//
+//  Created by Nolan Carroll on 4/23/15.
+//  Copyright (c) 2015 Medidata Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MORK.
+FOUNDATION_EXPORT double MORKVersionNumber;
+
+//! Project version string for MORK.
+FOUNDATION_EXPORT const unsigned char MORKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MORK/PublicHeader.h>
+
+
